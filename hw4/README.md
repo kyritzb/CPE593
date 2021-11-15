2b96dc4605f5e27c6a098533e1a7492a11149e89
Bryan Kyritz <bryan@securemeeting.org>
2021-11-15 13:54:12 -0500

    did half of hw4

---
+++ hw4/README.md
@@ -0,0 +1,15 @@
+# Dictionary
+
+IWrite a class HashMap using linear chaining that loads in a dictionary (given in https://drive.google.com/drive/folders/0Bwxfq4Y7f7vkTkZnVEFEcXVEWmc?resourcekey=0-mOXByawNd1BgAZIqFmdGzQ&usp=sharing (Links to an external site.)
+
+Then open the file "spell.txt"
+
+for example:
+testing this iz teste atttest
+apple apple John
+
+print out all the words not in the dictionary: iz, teste, atttest
+
+print out the histogram of the hash map so we can see how many bins have 0, 1, 2, 3, 4, 5, 6, 7, 8 or 9 or more collisions
+
+Then write the same functionality using a Trie and repeat. You can put all this in a single program
---
+++ hw4/dict.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct node
+{
+    string data;
+    node *next;
+};
+
+class LinkedList
+{
+private:
+    node *head, *tail;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void add_node(string n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+    }
+
+    void display()
+    {
+        node *current = head;
+
+        while (current != nullptr)
+        {
+            cout << current->data << endl;
+            current = current->next;
+        }
+    }
+
+    bool has(string search)
+    {
+        node *current = head;
+
+        while (current != nullptr)
+        {
+            if (current->data == search)
+            {
+                return true;
+            }
+
+            current = current->next;
+        }
+
+        return false;
+    }
+};
+
+class HashMap
+{
+
+    //array of linkedLists
+    LinkedList **arr;
+    int capacity;
+    // current size
+    int size;
+
+private:
+    void buildDictionary(string dictionaryFile)
+    {
+        ifstream is(dictionaryFile);
+        string line;
+
+        while (getline(is, line))
+        {
+            insertWord(line);
+        }
+    }
+
+public:
+    HashMap(string dictionaryFile)
+    {
+        cout << "Created new dictionary with " << dictionaryFile << endl;
+        // Initial capacity of hash array
+        capacity = 30;
+        size = 0;
+        //array of buckets
+        arr = new LinkedList *[capacity];
+
+        for (int i = 0; i < capacity; i++)
+            arr[i] = new LinkedList();
+
+        buildDictionary(dictionaryFile);
+    }
+    // This implements hash function to find index
+    // for a key
+    int hashCode(string key)
+    {
+        //return key % capacity;
+        int hashed = (int)key.length();
+
+        return hashed;
+    }
+
+    void insertWord(string newWord)
+    {
+        int hashIndex = hashCode(newWord);
+        LinkedList *list = arr[hashIndex];
+        list->add_node(newWord);
+    }
+
+    bool isWord(string word)
+    {
+        int hashIndex = hashCode(word);
+        LinkedList *list = arr[hashIndex];
+        return list->has(word);
+    }
+
+    void display()
+    {
+        for (int i = 0; i < capacity; i++)
+        {
+            cout << "--------Linked List #" << i + 1 << "--------" << endl;
+            LinkedList *list = arr[i];
+
+            list->display();
+        }
+    }
+};
+
+int main()
+{
+    cout << "running" << endl;
+    HashMap dict("dict.txt");
+    //dict.display();
+    return 0;
+}
